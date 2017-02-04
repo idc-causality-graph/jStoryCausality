f079fba33f9947f3b586b2500f5423c94667928a
Yonatan Graber <yonatan.graber@gmail.com>
2017-02-04 23:18:48 +0200

    Fixed bug in hit-storage TMI

--- src/main/java/il/ac/idc/yonatan/causality/contexttree/NodeLevel.java
+++ src/main/java/il/ac/idc/yonatan/causality/contexttree/NodeLevel.java
@@ -1,16 +1,20 @@
 package il.ac.idc.yonatan.causality.contexttree;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.ANY;
+import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.NONE;
 import static java.util.stream.Collectors.toList;
 
 /**
  * Created by ygraber on 1/28/17.
  */
 @Data
+@JsonAutoDetect(getterVisibility = NONE, setterVisibility = NONE, isGetterVisibility = NONE, fieldVisibility = ANY)
 public class NodeLevel {
     static ContextTree contextTree;
     private List<String> nodeIds = new ArrayList<>();
